dc3250dc1ae0a2057df3f6b6bb6a61c528e843db
sabrinawawara-afk <sabrina.wawara@gmail.com>
2026-09-14 21:44:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
 
+import os
+import discord
+
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot ist online als {bot.user}")
+
+bot.run(os.environ["DISCORD_TOKEN"])
